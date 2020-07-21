5a6f95f116cda38d0164c7bc83c5772f4f4f9061
akarnokd <akarnokd@gmail.com>
2020-07-21 12:12:05 +0200

    Use explicit path

--- usejdk16.sh
+++ usejdk16.sh
@@ -3,8 +3,8 @@
 # doesn't work for some reason, fails with tar exited with code 2
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
-# eval "$JDK_NEXT/bin/java -version"
 
 wget https://download.java.net/java/early_access/jdk16/6/GPL/openjdk-16-ea+6_linux-x64_bin.tar.gz
 tar -zxvf openjdk-16-ea+6_linux-x64_bin.tar.gz
-export JDK_NEXT="jdk-16"
+export JDK_NEXT="/home/travis/jdk-16"
+eval "$JDK_NEXT/bin/java -version"
