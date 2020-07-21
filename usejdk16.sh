ab3982287c13c4489a077707232c3c43a010d8ec
akarnokd <akarnokd@gmail.com>
2020-07-21 12:03:00 +0200

    Manually download and extract JDK 16

--- usejdk16.sh
+++ usejdk16.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
-echo JDK_NEXT: $JDK_NEXT
-eval "$JDK_NEXT/bin/java -version"
+
+# doesn't work for some reason, fails with tar exited with code 2
+# export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
+# echo JDK_NEXT: $JDK_NEXT
+# eval "$JDK_NEXT/bin/java -version"
+
+wget https://download.java.net/java/early_access/jdk16/6/GPL/openjdk-16-ea+6_linux-x64_bin.tar.gz
+tar -zxvf openjdk-16-ea+6_linux-x64_bin.tar.gz
+export JDK_NEXT="jdk-16"
