b6c383888f491f9cbd84207d2ad7549c5c4f2509
akarnokd <akarnokd@gmail.com>
2020-11-13 17:14:51 +0100

    Use 16b24

--- usejdk16.sh
+++ usejdk16.sh
@@ -4,7 +4,7 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget https://download.java.net/java/early_access/jdk16/22/GPL/openjdk-16-ea+22_linux-x64_bin.tar.gz
-tar -zxvf openjdk-16-ea+22_linux-x64_bin.tar.gz
+wget https://download.java.net/java/early_access/jdk16/24/GPL/openjdk-16-ea+24_linux-x64_bin.tar.gz
+tar -zxvf openjdk-16-ea+24_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-16
 eval "$JDK_NEXT/bin/java -version"
