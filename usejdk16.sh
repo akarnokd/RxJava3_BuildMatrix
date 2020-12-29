77df5fa31d227307773897f3390d5b4f6f69cb79
David Karnok <akarnokd@gmail.com>
2020-12-29 09:46:35 +0100

    Update usejdk16.sh

--- usejdk16.sh
+++ usejdk16.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget https://download.java.net/java/early_access/jdk16/28/GPL/openjdk-16-ea+28_linux-x64_bin.tar.gz
-tar -zxvf openjdk-16-ea+28_linux-x64_bin.tar.gz
+wget https://download.java.net/java/early_access/jdk16/30/GPL/openjdk-16-ea+30_linux-x64_bin.tar.gz
+tar -zxvf openjdk-16-ea+30_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-16
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
