5fa3e8deff853f81a329f3cf387b7509c59698d5
akarnokd <akarnokd@gmail.com>
2021-01-12 10:47:35 +0100

    Fix directory

--- usejdk17.sh
+++ usejdk17.sh
@@ -6,6 +6,6 @@
 
 wget https://download.java.net/java/early_access/jdk17/4/GPL/openjdk-17-ea+4_linux-x64_bin.tar.gz
 tar -zxvf openjdk-17-ea+4_linux-x64_bin.tar.gz
-export JDK_NEXT=$PWD/jdk-16
+export JDK_NEXT=$PWD/jdk-17
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
