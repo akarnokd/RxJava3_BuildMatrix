46dfe650fda47235626e1f961947bd3adc35745c
akarnokd <akarnokd@gmail.com>
2021-04-22 09:32:43 +0200

    Upgrade to JDK 17b18

--- usejdk17.sh
+++ usejdk17.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget https://download.java.net/java/early_access/jdk17/16/GPL/openjdk-17-ea+16_linux-x64_bin.tar.gz
-tar -zxvf openjdk-17-ea+16_linux-x64_bin.tar.gz
+wget https://download.java.net/java/early_access/jdk17/18/GPL/openjdk-17-ea+18_linux-x64_bin.tar.gz
+tar -zxvf openjdk-17-ea+18_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-17
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
