067061a6098007b132badf13ad920f8bbca632d7
akarnokd <akarnokd@gmail.com>
2021-06-29 16:09:27 +0200

    Fix Loom Override

--- use_loom.sh
+++ use_loom.sh
@@ -2,6 +2,6 @@
 
 wget https://download.java.net/java/early_access/loom/7/openjdk-17-loom+7-342_linux-x64_bin.tar.gz
 tar -zxvf openjdk-17-loom+7-342_linux-x64_bin.tar.gz
-export LOOM_JDK=jdk-17
-eval "$LOOM_JDK/bin/java -version"
-echo "LOOM_JDK=$LOOM_JDK" >> $GITHUB_ENV
+export JDK_NEXT=jdk-17
+eval "$JDK_NEXT/bin/java -version"
+echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
