5f51be6483a5bd0d8e47caf9287abb9ed87731de
David Karnok <akarnokd@gmail.com>
2021-08-08 12:58:33 +0200

    Update usejdk17.sh

--- usejdk17.sh
+++ usejdk17.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget -nv https://download.java.net/java/early_access/jdk17/31/GPL/openjdk-17-ea+31_linux-x64_bin.tar.gz
-tar -zxvf openjdk-17-ea+31_linux-x64_bin.tar.gz
+wget -nv https://download.java.net/java/GA/jdk17/0d483333a00540d886896bac774ff48b/35/GPL/openjdk-17_linux-x64_bin.tar.gz
+tar -zxvf openjdk-17_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-17
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
