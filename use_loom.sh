5a68f4886a3c6c1f4c7d0d8105457eda887b168c
akarnokd <akarnokd@gmail.com>
2021-08-17 14:48:03 +0200

    Restore Loom workaround

--- use_loom.sh
+++ use_loom.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-wget -nv https://download.java.net/java/early_access/loom/7/openjdk-17-loom+7-342_linux-x64_bin.tar.gz
-tar -zxvf openjdk-17-loom+7-342_linux-x64_bin.tar.gz
-export JDK_NEXT=${PWD}/jdk-17
+wget -nv https://download.java.net/java/early_access/loom/2/openjdk-18-loom+2-74_linux-x64_bin.tar.gz
+tar -zxvf openjdk-18-loom+2-74_linux-x64_bin.tar.gz
+export JDK_NEXT=${PWD}/jdk-18
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
 
-echo "Using older Gradle"
-mv -f gradle/wrapper/gradle-wrapper_loom.properties gradle/wrapper/gradle-wrapper.properties
-echo "Using Loom gradle.build"
-mv -f build_loom.gradle build.gradle
+#echo "Using older Gradle"
+#mv -f gradle/wrapper/gradle-wrapper_loom.properties gradle/wrapper/gradle-wrapper.properties
+#echo "Using Loom gradle.build"
+#mv -f build_loom.gradle build.gradle
