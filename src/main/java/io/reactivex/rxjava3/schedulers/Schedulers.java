892b7149daa667ec424b1024bb35f834121f6a71
akarnokd <akarnokd@gmail.com>
2021-08-17 14:38:39 +0200

    Upgrade to Gradle 7.2.0

--- src/main/java/io/reactivex/rxjava3/schedulers/Schedulers.java
+++ src/main/java/io/reactivex/rxjava3/schedulers/Schedulers.java
@@ -27,6 +27,8 @@ import io.reactivex.rxjava3.plugins.RxJavaPlugins;
  * The initial and runtime values of the various scheduler types can be overridden via the
  * {@code RxJavaPlugins.setInit(scheduler name)SchedulerHandler()} and
  * {@code RxJavaPlugins.set(scheduler name)SchedulerHandler()} respectively.
+ * Note that overriding any initial {@code Scheduler} via the {@link RxJavaPlugins}
+ * has to happen before the {@code Schedulers} class is accessed.
  * <p>
  * <strong>Supported system properties ({@code System.getProperty()}):</strong>
  * <ul>
@@ -376,6 +378,10 @@ public final class Schedulers {
      * execute those tasks "unexpectedly".
      * <p>
      * Note that this method returns a new {@code Scheduler} instance, even for the same {@code Executor} instance.
+     * <p>
+     * It is possible to wrap an {@code Executor} into a {@code Scheduler} without triggering the initialization of all the
+     * standard schedulers by using the {@link RxJavaPlugins#createExecutorScheduler(Executor, boolean, boolean)} method
+     * before the {@code Schedulers} class itself is accessed.
      * @param executor
      *          the executor to wrap
      * @return the new {@code Scheduler} wrapping the {@code Executor}
@@ -383,7 +389,7 @@ public final class Schedulers {
      */
     @NonNull
     public static Scheduler from(@NonNull Executor executor) {
-        return new ExecutorScheduler(executor, false, false);
+        return from(executor, false, false);
     }
 
     /**
@@ -450,10 +456,14 @@ public final class Schedulers {
      * execute those tasks "unexpectedly".
      * <p>
      * Note that this method returns a new {@code Scheduler} instance, even for the same {@code Executor} instance.
+     * <p>
+     * It is possible to wrap an {@code Executor} into a {@code Scheduler} without triggering the initialization of all the
+     * standard schedulers by using the {@link RxJavaPlugins#createExecutorScheduler(Executor, boolean, boolean)} method
+     * before the {@code Schedulers} class itself is accessed.
      * <p>History: 2.2.6 - experimental
      * @param executor
      *          the executor to wrap
-     * @param interruptibleWorker if {@code true} the tasks submitted to the {@link io.reactivex.rxjava3.core.Scheduler.Worker Scheduler.Worker} will
+     * @param interruptibleWorker if {@code true}, the tasks submitted to the {@link io.reactivex.rxjava3.core.Scheduler.Worker Scheduler.Worker} will
      * be interrupted when the task is disposed.
      * @return the new {@code Scheduler} wrapping the {@code Executor}
      * @since 3.0.0
@@ -461,7 +471,7 @@ public final class Schedulers {
      */
     @NonNull
     public static Scheduler from(@NonNull Executor executor, boolean interruptibleWorker) {
-        return new ExecutorScheduler(executor, interruptibleWorker, false);
+        return from(executor, interruptibleWorker, false);
     }
 
     /**
@@ -530,11 +540,16 @@ public final class Schedulers {
      * execute those tasks "unexpectedly".
      * <p>
      * Note that this method returns a new {@code Scheduler} instance, even for the same {@code Executor} instance.
+     * <p>
+     * It is possible to wrap an {@code Executor} into a {@code Scheduler} without triggering the initialization of all the
+     * standard schedulers by using the {@link RxJavaPlugins#createExecutorScheduler(Executor, boolean, boolean)} method
+     * before the {@code Schedulers} class itself is accessed.
+     *
      * @param executor
      *          the executor to wrap
-     * @param interruptibleWorker if {@code true} the tasks submitted to the {@link io.reactivex.rxjava3.core.Scheduler.Worker Scheduler.Worker} will
+     * @param interruptibleWorker if {@code true}, the tasks submitted to the {@link io.reactivex.rxjava3.core.Scheduler.Worker Scheduler.Worker} will
      * be interrupted when the task is disposed.
-     * @param fair if {@code true} tasks submitted to the will be executed by the underlying {@code Executor} one after the other, still
+     * @param fair if {@code true}, tasks submitted to the {@code Scheduler} or {@code Worker} will be executed by the underlying {@code Executor} one after the other, still
      * in a FIFO and non-overlapping manner, but allows interleaving with other tasks submitted to the underlying {@code Executor}.
      * If {@code false}, the underlying FIFO scheme will execute as many tasks as it can before giving up the underlying {@code Executor} thread.
      * @return the new {@code Scheduler} wrapping the {@code Executor}
@@ -542,7 +557,7 @@ public final class Schedulers {
      */
     @NonNull
     public static Scheduler from(@NonNull Executor executor, boolean interruptibleWorker, boolean fair) {
-        return new ExecutorScheduler(executor, interruptibleWorker, fair);
+        return RxJavaPlugins.createExecutorScheduler(executor, interruptibleWorker, fair);
     }
 
     /**
