45d4b810da87b8171e944d9321d38af59f829572
David Karnok <akarnokd@gmail.com>
2022-02-28 10:54:18 +0100

    Update usejdk18.sh

--- usejdk18.sh
+++ usejdk18.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget -nv https://download.java.net/java/early_access/jdk18/33/GPL/openjdk-18-ea+33_linux-x64_bin.tar.gz
-tar -zxvf openjdk-18-ea+33_linux-x64_bin.tar.gz
+wget -nv https://download.java.net/java/GA/jdk18/43f95e8614114aeaa8e8a5fcf20a682d/36/GPL/openjdk-18_linux-x64_bin.tar.gz
+tar -zxvf openjdk-18_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-18
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
