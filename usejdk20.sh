57f7e581ef3309172fe47c431de835f7e409d58c
David Karnok <akarnokd@gmail.com>
2023-01-25 08:44:53 +0100

    Update usejdk20.sh

--- usejdk20.sh
+++ usejdk20.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget -nv https://download.java.net/java/early_access/jdk20/27/GPL/openjdk-20-ea+27_linux-x64_bin.tar.gz
-tar -zxvf openjdk-20-ea+27_linux-x64_bin.tar.gz
+wget -nv https://download.java.net/java/early_access/jdk20/32/GPL/openjdk-20-ea+32_linux-x64_bin.tar.gz
+tar -zxvf openjdk-20-ea+32_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-20
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
