00ecbf23140f64606c4d0ebf5701a853d11d6382
David Karnok <akarnokd@gmail.com>
2023-02-15 14:26:12 +0100

    Update use_loom.sh

--- use_loom.sh
+++ use_loom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget -nv https://download.java.net/java/early_access/jdk20/32/GPL/openjdk-20-ea+32_linux-x64_bin.tar.gz
-tar -zxvf openjdk-20-ea+32_linux-x64_bin.tar.gz
+wget -nv https://download.java.net/java/GA/jdk20/bdc68b4b9cbc4ebcb30745c85038d91d/36/GPL/openjdk-20_linux-x64_bin.tar.gz
+tar -zxvf openjdk-20_linux-x64_bin.tar.gz
 export JDK_NEXT=${PWD}/jdk-20
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
