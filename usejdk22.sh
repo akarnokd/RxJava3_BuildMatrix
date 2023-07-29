a28baf43bf11a1aec875485346ca18cc9095dafb
David Karnok <akarnokd@gmail.com>
2023-07-29 10:58:53 +0200

    Update usejdk22.sh

--- usejdk22.sh
+++ usejdk22.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget -nv https://download.java.net/java/early_access/jdk22/4/GPL/openjdk-22-ea+4_linux-x64_bin.tar.gz
-tar -zxvf openjdk-22-ea+4_linux-x64_bin.tar.gz
+wget -nv https://download.java.net/java/early_access/jdk22/8/GPL/openjdk-22-ea+8_linux-x64_bin.tar.gz
+tar -zxvf openjdk-22-ea+8_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-22
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
