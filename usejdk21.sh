db893e67ceac95abbab0dfe80c9bdc2ee23bb732
David Karnok <akarnokd@gmail.com>
2023-11-28 12:08:29 +0100

    Update usejdk21.sh

--- usejdk21.sh
+++ usejdk21.sh
@@ -6,6 +6,6 @@
 
 wget -nv https://download.java.net/java/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_linux-x64_bin.tar.gz
 tar -zxvf openjdk-21.0.1_linux-x64_bin.tar.gz
-export JDK_NEXT=$PWD/jdk-21
+export JDK_NEXT=$PWD/jdk-21.0.1
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
