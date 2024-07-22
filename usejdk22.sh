1511132e9797049801743e5642dd5a8e4047f4ce
akarnokd <akarnokd@gmail.com>
2024-07-22 10:47:59 +0200

    Fix 22.0.2 dir

--- usejdk22.sh
+++ usejdk22.sh
@@ -6,6 +6,6 @@
 
 wget -nv https://download.java.net/java/GA/jdk22.0.2/c9ecb94cd31b495da20a27d4581645e8/9/GPL/openjdk-22.0.2_linux-x64_bin.tar.gz
 tar -zxvf openjdk-22.0.2_linux-x64_bin.tar.gz
-export JDK_NEXT=$PWD/jdk-22
+export JDK_NEXT=$PWD/jdk-22.0.2
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
