99ee8fbdb25fd5d881aa0d7e2dded0c158610e21
David Karnok <akarnokd@gmail.com>
2024-08-24 20:43:14 +0200

    Update usejdk23.sh

--- usejdk23.sh
+++ usejdk23.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget -nv https://download.java.net/java/early_access/jdk23/33/GPL/openjdk-23-ea+33_linux-x64_bin.tar.gz
-tar -zxvf openjdk-23-ea+33_linux-x64_bin.tar.gz
+wget -nv https://download.java.net/java/GA/jdk23/3c5b90190c68498b986a97f276efd28a/37/GPL/openjdk-23_linux-x64_bin.tar.gz
+tar -zxvf openjdk-23_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-23
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
