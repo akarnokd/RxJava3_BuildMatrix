5e85ff316b7f3b81d5b810233cc6bdaab3cfb231
David Karnok <akarnokd@gmail.com>
2025-04-08 08:20:34 +0200

    JDK 25b17

--- usejdk25.sh
+++ usejdk25.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget -nv https://download.java.net/java/early_access/jdk25/9/GPL/openjdk-25-ea+9_linux-x64_bin.tar.gz
-tar -zxvf openjdk-25-ea+9_linux-x64_bin.tar.gz
+wget -nv https://download.java.net/java/early_access/jdk25/17/GPL/openjdk-25-ea+17_linux-x64_bin.tar.gz
+tar -zxvf openjdk-25-ea+17_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-25
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
