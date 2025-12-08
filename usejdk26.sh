84d80107aae664f41ccda5dccae6294407d0225f
David Karnok <akarnokd@gmail.com>
2025-12-08 10:59:24 +0100

    Update usejdk26.sh 26b27

--- usejdk26.sh
+++ usejdk26.sh
@@ -4,8 +4,8 @@
 # export JDK_NEXT=$(./install-jdk.sh --silent --emit-java-home -F 16 -L GPL)
 # echo JDK_NEXT: $JDK_NEXT
 
-wget -nv https://download.java.net/java/early_access/jdk26/16/GPL/openjdk-26-ea+16_linux-x64_bin.tar.gz
-tar -zxvf openjdk-26-ea+16_linux-x64_bin.tar.gz
+wget -nv https://download.java.net/java/early_access/jdk26/27/GPL/openjdk-26-ea+27_linux-x64_bin.tar.gz
+tar -zxvf openjdk-26-ea+27_linux-x64_bin.tar.gz
 export JDK_NEXT=$PWD/jdk-26
 eval "$JDK_NEXT/bin/java -version"
 echo "JDK_NEXT=$JDK_NEXT" >> $GITHUB_ENV
